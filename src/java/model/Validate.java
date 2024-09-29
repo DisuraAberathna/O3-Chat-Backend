ca2bb0f5d39c43110b2b59751071713fa2ce6b73
Disura Chathwara <disura2005@gmail.com>
2024-09-29 20:35:54 +0530

    create and programmed Validate model

---
+++ src/java/model/Validate.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author SINGER
+ */
+public class Validate {
+
+    public static boolean hasDigit(String text) {
+        return text.matches("\\d+");
+    }
+
+    public static boolean isValidMobile(String text) {
+        return text.matches("^07[01245678]{1}[0-9]{7}$");
+    }
+
+    public static boolean isValidEmail(String text) {
+        return text.matches("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
+    }
+
+    public static boolean isValidPassword(String text) {
+        return text.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$");
+    }
+}
